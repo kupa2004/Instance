3b39cf795e21e095cc6917b6bb7d23465cebd2b6
kupa2004 <98742842+kupa2004@users.noreply.github.com>
2022-03-09 16:44:14 +0300

    Create congif.tf

---
+++ congif.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = file("cred.json")
+  project = "solar-semiotics-343520"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+output "gcp_instance_ip" {
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-test14"
+  machine_type = "f1-micro"
+  zone         = "us-west1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  tags = [
+      "http-server"
+    ]
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  
+  #metadata ={
+  # "startup-script" = <<EOT
+  #!/bin/bash
+  #EOT}
+}  
+  
